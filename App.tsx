13b4e5ed5c2d0beae6a1408d4105529d6b47a0db
Wellington Dos Santos <wellington30071997@gmail.com>
2024-02-02 21:10:13 -0300

    feat: add routes system

--- App.tsx
+++ App.tsx
@@ -1,20 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { Home } from './src/screens/Home';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const Stack = createStackNavigator();
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const App: React.FC = () => (
+  <NavigationContainer>
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="Home" component={Home} />
+    </Stack.Navigator>
+  </NavigationContainer>
+);
+
+export default App;
---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+const Home: React.FC = () => {
+    return (
+        <SafeAreaView>
+            <View>
+                <Text>Hello world!</Text>
+            </View>
+        </SafeAreaView>
+    );
+};
+
+export { Home };
