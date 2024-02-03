289d04ed5449e8ffa1f5f818084340446fae32b9
Wellington Dos Santos <wellington30071997@gmail.com>
2024-02-03 08:57:55 -0300

    checkpoint: create ListCategories component

--- src/screens/Home/components/index.ts
+++ src/screens/Home/components/index.ts
@@ -2,40 +2,54 @@ import { StatusBar, TextInput } from 'react-native';
 import styled from 'styled-components/native';
 
 export const Wrapper = styled.View`
-    background-color: ${({ theme }) => theme.color.white};
-    flex: 1;
+  background-color: ${({ theme }) => theme.color.white};
+  flex: 1;
 `;
 
 export const Header = styled.View`
-    background-color: ${({ theme }) => theme.color.primary};
-    padding-top: ${StatusBar.currentHeight}px;
+  background-color: ${({ theme }) => theme.color.primary};
+  padding-top: ${StatusBar.currentHeight}px;
 `;
 
 export const Nav = styled.View`
-    flex-direction: row;
-    justify-content: space-between;
-    padding: 0 20px;
+  flex-direction: row;
+  justify-content: space-between;
+  padding: 0 20px;
 `;
 
 export const HeaderTitle = styled.Text`
-    font-size: 26px;
-    font-weight: bold;
-    padding: 0 20px;
-    padding-top: 20px;
-    color: ${({ theme }) => theme.color.white};
+  font-size: 26px;
+  font-weight: bold;
+  padding: 0 20px;
+  padding-top: 20px;
+  color: ${({ theme }) => theme.color.white};
 `;
 
 export const InputContainer = styled.View`
-    height: 40px;
-    border: none;
-    elevation: 12;
-    margin: 0 20px;
-    padding: 0 10px;
-    gap: 5px;
-    border-radius: 10px;
-    align-items: center;
-    transform: translateY(20px);
-    position: relative;
-    flex-direction: row;
-    background-color: ${({ theme }) => theme.color.white};
+  height: 40px;
+  border: none;
+  elevation: 12;
+  margin: 0 20px;
+  padding: 0 10px;
+  gap: 5px;
+  border-radius: 10px;
+  align-items: center;
+  transform: translateY(20px);
+  position: relative;
+  flex-direction: row;
+  background-color: ${({ theme }) => theme.color.white};
+`;
+
+export const ListCategories = styled.View`
+  margin: 40px 20px;
+  justify-content: space-between;
+  flex-direction: row;
+`;
+
+export const CategoryIcon = styled.View`
+  padding: 10px;
+  border-radius: 10px;
+  justify-content: center;
+  align-items: center;
+  background-color: ${({ theme }) => theme.color.secondary};
 `;
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,28 +1,39 @@
 import React from 'react';
-import { StatusBar, TextInput } from 'react-native';
+import { StatusBar, TextInput, View } from 'react-native';
 import { MaterialCommunityIcons, MaterialIcons } from '@expo/vector-icons';
 
-import { Wrapper, Header, HeaderTitle, Nav, InputContainer } from './components';
+import theme from '../../theme';
+import { Wrapper, Header, HeaderTitle, Nav, InputContainer, ListCategories, CategoryIcon } from './components';
+
+const categoryIcons = [
+  <MaterialIcons name="flight" size={24} color={theme.color.primary} />,
+  <MaterialIcons name="beach-access" size={24} color={theme.color.primary} />,
+  <MaterialIcons name="near-me" size={24} color={theme.color.primary} />,
+  <MaterialIcons name="place" size={24} color={theme.color.primary} />,
+];
 
 const Home: React.FC = () => {
-    return (
-        <Wrapper>
-            <StatusBar translucent backgroundColor="rgba(0,0,0,0)" />
-            <Header>
-                <Nav>
-                    <MaterialCommunityIcons name="sort-variant" size={24} color="white" />
-                    <MaterialIcons name="notifications-none" size={24} color="white" />
-                </Nav>
-                <HeaderTitle>
-                    Explore the{'\n'}beautiful places
-                </HeaderTitle>
-                <InputContainer>
-                    <MaterialIcons name="search" size={24} color="black" />
-                    <TextInput placeholder='Search places' />
-                </InputContainer>
-            </Header>
-        </Wrapper>
-    );
+  return (
+    <Wrapper>
+      <StatusBar translucent backgroundColor="rgba(0,0,0,0)" />
+      <Header>
+        <Nav>
+          <MaterialCommunityIcons name="sort-variant" size={24} color="white" />
+          <MaterialIcons name="notifications-none" size={24} color="white" />
+        </Nav>
+        <HeaderTitle>
+          Explore the{'\n'}beautiful places
+        </HeaderTitle>
+        <InputContainer>
+          <MaterialIcons name="search" size={24} color="black" />
+          <TextInput placeholder='Search places' />
+        </InputContainer>
+      </Header>
+      <ListCategories>
+        {categoryIcons.map((icon, index) => <CategoryIcon key={index}>{icon}</CategoryIcon>)}
+      </ListCategories>
+    </Wrapper>
+  );
 };
 
 export { Home };
